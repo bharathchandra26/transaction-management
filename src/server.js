38fc01a2c4a45dfe771a831d781e92a29573fd6e
bharathchandra26 <bharathchandra830@gmail.com>
2024-11-21 21:46:00 +0530

    Initial commit

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -9,9 +10,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Use the MongoDB URI from environment variables
-const mongoUri = process.env.MONGODB_URI;
 
+const mongoUri = 'mongodb+srv://USER:user123@cluster0.2tfvz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 if (!mongoUri) {
   console.error('MongoDB URI is not defined in the .env file!');
   process.exit(1);  // Exit the app if MongoDB URI is not defined
@@ -34,3 +34,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
